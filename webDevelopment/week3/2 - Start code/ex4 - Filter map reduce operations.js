247bb5afeefd35f822a45546c9b25973bf0d3d03
VorngViseth <vorngviseth077@gmail.com>
2026-01-29 10:54:55 +0700

    feat: w3 ng hx ng hx

---
+++ webDevelopment/week3/2 - Start code/ex4 - Filter map reduce operations.js
@@ -0,0 +1,27 @@
+let numbers = [1, 2, 3, 4, 5];
+
+// Write the function to filter out even numbers from an array
+// You need to use the array.filter() function
+function filterEvenNumbers(arr) {
+  // TODO - Your code here (1 line)
+  return arr.filter(m => m % 2 == 0);
+  
+}
+
+// Function to map array elements to their squared values
+// You need to use the array.map() function
+function squareNumbers(arr) {
+  //  TODO - Your code here (1 line)
+  return arr.map(m => m * m);
+}
+
+// Function to compute the sum of array elements
+// You need to use the array.reduce() function
+function sumArray(arr) {
+  //  TODO - Your code here (1 line)
+  return arr.reduce((total, m ) => total += m);
+}
+
+console.log("Even numbers:", filterEvenNumbers(numbers)); // [2, 4]
+console.log("Squared numbers:", squareNumbers(numbers)); // [1, 4, 9, 16, 25]
+console.log("Sum of numbers:", sumArray(numbers)); // 15
